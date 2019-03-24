22b7e19584bda584a0dab7cca20ae0f28240f4a0
Bin <iamsk.info@gmail.com>
2019-03-24 22:34:14 +0800

    bugfix

--- applications/crawler/management/commands/rawpoint2point.py
+++ applications/crawler/management/commands/rawpoint2point.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
-import djclick as click
 import json
+import djclick as click
+from django.db.models import Model
 
 from applications.crawler.models import RawPoint
 from applications.web.models import Point
@@ -10,15 +11,16 @@ from applications.web.models import Point
 @click.option('--source', help='The source to do.')
 def command(source):
     raw_points = RawPoint.objects.filter(status=1, source=source)
-    print raw_points
     for raw_point in raw_points:
         if not raw_point.address or len(raw_point.address) == 0:
             continue
         data = json.loads(raw_point.raw_data)
-        p, created = Point.objects.get_or_create(address=raw_point.address)
-        if not created:
+        try:
+            Point.objects.get(address=raw_point.address)
             print 'same address: {}'.format(raw_point.address)
             continue
+        except Model.DoesNotExist:
+            p = Point()
         if source == 1:
             p.province = data['province']
             p.city = data['city']
