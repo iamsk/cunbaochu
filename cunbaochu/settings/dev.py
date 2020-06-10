960586e3aae6f016e944cbd263c3e9479845f492
bin <iamsk.info@gmail.com>
2020-06-10 14:54:24 +0800

    remove unused config

--- cunbaochu/settings/dev.py
+++ cunbaochu/settings/dev.py
@@ -1,16 +1,5 @@
 from .common import *
 
-DATABASES['default'] = {
-    'ENGINE': 'django.db.backends.mysql',
-    'NAME': 'test2',
-    'USER': 'test',
-    'PASSWORD': 'Dev@hfq',
-    'HOST': '123.56.135.63',
-    'PORT': '3306',
-    'OPTIONS': {
-    },
-}
-
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "../static"),
 )
